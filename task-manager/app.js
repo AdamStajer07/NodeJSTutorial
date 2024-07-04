c3d562b1d4b854580254c5a35ad393b47b7fb33d
AdamStajer07 <stajeradam9@gmail.com>
2024-07-04 19:36:24 +0200

    Task manager from FreeCodeCamp. 2/2

--- task-manager/app.js
+++ task-manager/app.js
@@ -3,6 +3,8 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddlware = require('./middleware/error-handler')
 
 // middleware
 app.use(express.static('./public'))
@@ -10,14 +12,13 @@ app.use(express.json())
 
 
 // routes
-app.get('/hello', (req, res) => {
-    res.send('Task manager app')
-})
 
 app.use('/api/v1/tasks', tasks)
 
+app.use(notFound)
+app.use(errorHandlerMiddlware)
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
---
+++ task-manager/middleware/error-handler.js
@@ -0,0 +1,5 @@
+const errorHandlerMiddlware = (err, req, res, next) => {
+    console.log(err)
+    return res.status(err.status).json({msg:err.message})
+}
+module.exports = errorHandlerMiddlware
